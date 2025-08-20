148e5302d6c3b5e5beab435fff9b47870a3dbf58
Iuky.O <iumyfarias03@gmail.com>
2025-08-20 10:46:13 -0300

    fix: coletáveis mecânica

---
+++ climatic-koala/scripts/agua.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+var num_agua = 0
+
+func _ready() -> void:
+	$AnimatedSprite2D.frame = 0
+
+
+func _process(delta: float) -> void:
+	$"../CanvasLayer/total_agua".text = str("Sementes = ", num_agua)
+
+
+func _on_body_entered(body: Node2D) -> void:
+	#show()
+	#$AnimatedSprite2D.frame = 0
+	#
+	##faz crescer
+	#for i in range(5):
+		#await get_tree().create_timer(3.0).timeout
+		#$AnimatedSprite2D.frame = i+1
+		
+	
+	if visible:
+		num_agua += 1
+		$"../CanvasLayer/total_agua".text = str("Sementes = ", num_agua)
+		hide()
---
+++ climatic-koala/scripts/menina.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+const SPEED = 100.0
+const JUMP_VELOCITY = -400.0
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		pass
+		# velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Pega direção
+	var directionX := Input.get_axis("ui_left", "ui_right")
+	var directionY := Input.get_axis("ui_up", "ui_down")
+	var direction = Vector2(directionX, directionY).normalized()
+
+	# Movimento
+	if direction != Vector2.ZERO:
+		velocity.x = direction.x * SPEED
+		velocity.y = direction.y * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+		$AnimatedSprite2D.play("parada")
+
+	move_and_slide()
+
+	# Animações
+	if Input.is_action_pressed("ui_left"):
+		$AnimatedSprite2D.play("andando_esquerda")
+	elif Input.is_action_pressed("ui_right"):
+		$AnimatedSprite2D.play("andando_direita")
